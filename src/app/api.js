0a72bb0e5d98c2895e27785dda39c77f8ccbb2c6
AbhiGiri <abhishekmkumar003@gmali.com>
2023-04-01 23:22:43 +0530

    url changes in .env

--- src/app/api.js
+++ src/app/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const devEnv = process.env.NODE_ENV !== "production";
-console.log(process.env.NODE_ENV);
 const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
 const API = axios.create({
